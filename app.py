6e4c5154488a7e85647392e10e541ab4fd2891eb
sudhir1552005 <balisudhir1552005@gmail.com>
2026-03-10 16:13:01 +0530

    MongoDB + Render live monitoring

--- app.py
+++ app.py
@@ -4,34 +4,46 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# MongoDB connection
-client = MongoClient("mongodb+srv://balisudhir1552005_db_user:12345@cluster0.gy0d1dh.mongodb.net/smart_agro?retryWrites=true&w=majority&appName=Cluster0")
-
+# MongoDB Connection
+client = MongoClient("mongodb+srv://balisudhir1552005_db_user:12345@cluster0.gy0d1dh.mongodb.net/?retryWrites=true&w=majority")
 db = client["smart_agro"]
 collection = db["ph_data"]
 
-# Home page
+# Homepage
 @app.route("/")
 def home():
-    data = list(collection.find().sort("time",-1).limit(20))
+    data = list(collection.find().sort("time", -1).limit(20))
     return render_template("index.html", data=data)
 
-# ESP32 data receiver
+# API to receive sensor data
 @app.route("/sensor", methods=["POST"])
 def sensor():
 
-    data = request.get_json()
+    sensor_data = request.json
 
     record = {
-        "moisture": data["moisture"],
-        "ph": data["ph"],
+        "moisture": sensor_data["moisture"],
+        "ph": sensor_data["ph"],
         "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
 
     collection.insert_one(record)
 
-    return jsonify({"status":"success"})
+    return jsonify({"status":"data stored"})
 
+# SAMPLE DATA ROUTE
+@app.route("/sample")
+def sample():
+
+    sample_data = {
+        "moisture": 55,
+        "ph": 6.85,
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+    collection.insert_one(sample_data)
+
+    return "Sample data inserted!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
